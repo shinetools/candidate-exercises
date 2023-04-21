041fb733fe973808d8abcbc9950925e4312c7e8b
Julien Birgand <julien.birgand@gmail.com>
2023-04-21 16:31:19 +0200

    feat: update the test suites

--- __tests__/suites/uniqBy.js
+++ __tests__/suites/uniqBy.js
@@ -4,55 +4,109 @@ describe('uniqBy', () => {
   it('should deduplicate objects inside an array using the provided key', () => {
     expect(
       uniqBy('id', [
-        { id: 1, name: 'Anakin', age: 10 },
-        { id: 1, name: 'Darth Vader', side: 'dark' },
-      ]),
-    ).toEqual([{ id: 1, name: 'Anakin', age: 10 }]);
-  });
-  it('should deduplicate objects inside an array keeping the first one and preserving the sort order', () => {
-    expect(
-      uniqBy('id', [
-        { id: 1, name: 'Anakin', age: 10 },
+        { id: 2, name: 'Anakin', age: 10 },
         {
-          id: 3,
-          name: 'Luke',
-          side: 'light',
-          age: 50,
-        },
-        {
-          id: 2,
+          id: 0,
           name: 'Obi-Wan',
           side: 'light',
           age: 25,
         },
-        { id: 1, name: 'Darth Vader', side: 'dark' },
-        {
-          id: 4,
-          name: 'Yoda',
-          side: 'light',
-          age: 1000,
-        },
+        { id: 2, name: 'Darth Vader', side: 'dark' },
       ]),
     ).toEqual([
-      { id: 1, name: 'Anakin', age: 10 },
+      { id: 2, name: 'Anakin', age: 10 },
       {
-        id: 3,
-        name: 'Luke',
-        side: 'light',
-        age: 50,
-      },
-      {
-        id: 2,
+        id: 0,
         name: 'Obi-Wan',
         side: 'light',
         age: 25,
       },
+    ]);
+  });
+
+  it('should work with multiple duplicated objects', () => {
+    expect(
+      uniqBy('id', [
+        { id: 2, name: 'Anakin', age: 10 },
+        {
+          id: 0,
+          name: 'Dark Maul',
+        },
+        { id: 2, name: 'Darth Vader', side: 'dark' },
+        {
+          id: 0,
+          name: 'Obi-Wan',
+          side: 'light',
+          age: 25,
+        },
+        {
+          id: 1,
+          name: 'R2-D2',
+        },
+        {
+          id: 2,
+          name: 'Yoda',
+        },
+      ]),
+    ).toEqual([
+      { id: 2, name: 'Anakin', age: 10 },
       {
-        id: 4,
-        name: 'Yoda',
-        side: 'light',
-        age: 1000,
+        id: 0,
+        name: 'Dark Maul',
       },
+      {
+        id: 1,
+        name: 'R2-D2',
+      },
+    ]);
+  });
+
+  it('should work with a different key', () => {
+    expect(
+      uniqBy('uniqueKey', [
+        { uniqueKey: 2, name: 'Anakin', age: 10 },
+        {
+          uniqueKey: 0,
+          name: 'Obi-Wan',
+          side: 'light',
+          age: 25,
+        },
+        { uniqueKey: 2, name: 'Darth Vader', side: 'dark' },
+      ]),
+    ).toEqual([
+      { uniqueKey: 2, name: 'Anakin', age: 10 },
+      {
+        uniqueKey: 0,
+        name: 'Obi-Wan',
+        side: 'light',
+        age: 25,
+      },
+    ]);
+  });
+
+  it('should not mutate the array input', () => {
+    const arrayInput = [
+      { id: 2, name: 'Anakin', age: 10 },
+      {
+        id: 0,
+        name: 'Obi-Wan',
+        side: 'light',
+        age: 25,
+      },
+      { id: 2, name: 'Darth Vader', side: 'dark' },
+    ];
+
+    uniqBy('id', arrayInput);
+
+    expect(arrayInput).toEqual([
+      { id: 2, name: 'Anakin', age: 10 },
+      {
+        id: 0,
+        name: 'Obi-Wan',
+        side: 'light',
+        age: 25,
+      },
+      { id: 2, name: 'Darth Vader', side: 'dark' },
     ]);
   });
 });
